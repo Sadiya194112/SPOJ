9a9ead8a66202a27944d130a2747ddcc0e44279e
Sadiya194112 <59763617+Sadiya194112@users.noreply.github.com>
2023-01-28 22:55:36 +0600

    Map of pair and string

---
+++ ACMCEG2B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    int n;  cin>>n;
+    map<pair<int,int>,string>mp;
+    map<pair<int,int>,string>::iterator it;
+
+    while(n--){
+        int a,b;    string s;
+        cin>>a>>b>>s;
+        mp[{a,b}]=s;
+    }
+//    for(it=mp.begin(); it!=mp.end(); it++){
+//        cout<<it->first.first<<" "<<it->first.second<<" "<<it->second<<endl;
+//    }
+    int q;  cin>>q;
+    while(q--){
+        int a,b;    cin>>a>>b;
+        cout<<mp[{a,b}]<<endl;
+    }
+
+    return 0;
+}
